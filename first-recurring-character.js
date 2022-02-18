689e0ef83fb2dcfc3511d30e934ce2ef53d577b6
petitoff <petitoff@outlook.com>
2022-02-18 17:14:33 +0100

    Error explanation
    The first function (first solution) has the following problem. As we take numbers sequentially and check whether a given number has not repeated. So if we take the setting [2,5,1,2] then the answer will be 2 which is correct. However, if we take [2,5,5,2], the answer will also be 2, which is incorrect. The second function and the answer resists this drawback and works fine.

--- first-recurring-character.js
+++ first-recurring-character.js
@@ -22,4 +22,13 @@ function firstRecurringCharacter2(input){
 } // O(n)
 
 // console.log(firstRecurringCharacter([2,5,1,2,3,5,1,2,4]));
+/*
+    Error explanation
+    The first function (first solution) has the following problem. 
+    As we take numbers sequentially and check whether a given number 
+    has not repeated. So if we take the setting [2,5,1,2] then 
+    the answer will be 2 which is correct. However, 
+    if we take [2,5,5,2], the answer will also be 2, which is incorrect. 
+    The second function and the answer resists this drawback and works fine.
+*/
 console.log(firstRecurringCharacter2([2,5,1,2,3,5,1,2,4]));
