680e394aca22f3dd74e6444f44e1188497ff25a9
petitoff <petitoff@outlook.com>
2022-02-18 16:40:49 +0100

    Without Collision

--- my-hash-table.js
+++ my-hash-table.js
@@ -36,19 +36,29 @@ class HashTable {
   } 
 
   keys() {
-    const keysArrays = [];
-    for (let i=0; i < this.data.length; i++)
-    {
-      if(this.data[i]){
-        console.log(this.data[i][0][0]);
-        keysArrays.push(this.data[i][0][0]);
-      }
+    if (!this.data.length) {
+      return undefined
     }
-    return keysArrays;
+    let result = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+        // if it's not an empty memory cell
+        if (this.data[i] && this.data[i].length) {
+          // but also loop through all the potential collisions
+          if (this.data.length > 1) {
+            for (let j = 0; j < this.data[i].length; j++) {
+              result.push(this.data[i][j][0])
+            }
+          } else {
+            result.push(this.data[i][0])
+          } 
+        }
+    }
+    return result; 
   }
 }
   
-const myHashTable = new HashTable(50);
+const myHashTable = new HashTable(2);
 myHashTable.set('grapes', 10000);
 myHashTable.set('apples', 54);
 myHashTable.set('oranges', 2);
