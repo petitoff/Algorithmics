dc6af0e79ca2a26f0dfd691e1b91b77fe507e4ac
petitoff <petitoff@outlook.com>
2022-02-18 17:05:26 +0100

    Solving the problem with a hash table
    The problem is solved thanks to the hash table. The program works as follows. Successive numbers from the list are entered into the hash table as keys. Values are volatile, they are assigned as consecutive indexes. Then we check whether the given key does not exist anymore, if it exists, it means that the given number has already appeared. If not, the number is entered into the hash table.

--- first-recurring-character.js
+++ first-recurring-character.js
@@ -9,4 +9,17 @@ function firstRecurringCharacter(input) {
     return undefined;
 } // O(n^2)
 
-console.log(firstRecurringCharacter([2,5,1,2,3,5,1,2,4]));
+function firstRecurringCharacter2(input){
+    let map = {};
+    for (let i=0; i < input.length; i++){
+        if(map[input[i]] !== undefined) {
+            return input[i];
+        } else {
+            map[input[i]] = i;
+        }
+    }
+    return undefined;
+}
+
+// console.log(firstRecurringCharacter([2,5,1,2,3,5,1,2,4]));
+console.log(firstRecurringCharacter2([2,5,1,2,3,5,1,2,4]));
