dff6c268afde0481b97e959103b9bc9df77db25e
Błażej Domagała <petitoff@outlook.com>
2022-04-18 22:17:32 +0200

    Create Longest Substring Without Repeating Characters.js

---
+++ LeetCode/Longest Substring Without Repeating Characters.js
@@ -0,0 +1,27 @@
+/*
+Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+Description:
+Given a string s, find the length of the longest substring without repeating characters.
+*/
+
+const lengthOfLongestSubstring = function (s) {
+  const length = s.length;
+  if (length <= 1) {
+    return length;
+  }
+  let p = 0;
+  let q = 1;
+  let result = 1;
+  while (q < length) {
+    const stringSlice = s.slice(p, q);
+    const checkIfExists = stringSlice.indexOf(s[q]);
+    if (checkIfExists !== -1) {
+      p = p + checkIfExists + 1;
+    }
+    q++;
+    result = q - p > result ? q - p : result;
+  }
+  return result;
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
