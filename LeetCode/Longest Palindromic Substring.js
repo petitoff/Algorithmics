0837e46b9c86c632a26caa62ab4201f55270fc2e
petitoff <petitoff@outlook.com>
2022-04-30 12:52:18 +0200

    Create a Longest Palindromic Substring.js

---
+++ LeetCode/Longest Palindromic Substring.js
@@ -0,0 +1,31 @@
+/*
+Link: https://leetcode.com/problems/longest-palindromic-substring/
+Description:
+Given a string s, return the longest palindromic substring in s.
+*/
+
+const getDrome = function (left, right, s) {
+  while (left >= 0 && right < s.length) {
+    if (s[left] !== s[right]) break;
+    left--;
+    right++;
+  }
+
+  return [left + 1, right];
+};
+
+const longestPalindrome = function (s) {
+  let max = [0, 1];
+
+  for (let i = 0; i < s.length; i++) {
+    const even = getDrome(i - 1, i, s);
+    const odd = getDrome(i - 1, i + 1, s);
+
+    let curMax = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+    max = max[1] - max[0] > curMax[1] - curMax[0] ? max : curMax;
+  }
+
+  return s.slice(max[0], max[1]);
+};
+
+console.log(longestPalindrome('babad'));
