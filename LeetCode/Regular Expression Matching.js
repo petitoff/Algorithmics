343cce7a11edd48d8e0b21c9c50dbb006321472e
petitoff <petitoff@outlook.com>
2022-04-30 16:16:53 +0200

    Create solution

---
+++ LeetCode/Regular Expression Matching.js
@@ -0,0 +1,28 @@
+const isMatch = function (s, p) {
+  const dfs = (i, j, memo = {}) => {
+    const key = i + ',' + j;
+
+    if (key in memo) return memo[key]; // memo
+
+    if (i >= s.length && j >= p.length) return true;
+    if (j >= p.length) return false;
+
+    const match = i < s.length && (s[i] == p[j] || p[j] == '.');
+
+    if (j + 1 < p.length && p[j + 1] == '*') {
+      memo[key] = dfs(i, j + 2, memo) || (match && dfs(i + 1, j, memo));
+      return memo[key];
+    }
+    if (match) {
+      memo[key] = dfs(i + 1, j + 1, memo);
+      return memo[key];
+    }
+
+    memo[key] = false;
+    return false;
+  };
+
+  return dfs(0, 0);
+};
+
+console.log(isMatch('aa', 'a*'));
