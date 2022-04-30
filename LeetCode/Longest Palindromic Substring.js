a64343fadd9fa1e07c6258c11ed90d7eef161fb5
petitoff <petitoff@outlook.com>
2022-04-30 13:14:30 +0200

    Algorithm acceleration

--- LeetCode/Longest Palindromic Substring.js
+++ LeetCode/Longest Palindromic Substring.js
@@ -4,7 +4,7 @@ Description:
 Given a string s, return the longest palindromic substring in s.
 */
 
-const getDrome = function (left, right, s) {
+const getDrome = (left, right, s) => {
   while (left >= 0 && right < s.length) {
     if (s[left] !== s[right]) break;
     left--;
@@ -18,10 +18,10 @@ const longestPalindrome = function (s) {
   let max = [0, 1];
 
   for (let i = 0; i < s.length; i++) {
-    const even = getDrome(i - 1, i, s);
     const odd = getDrome(i - 1, i + 1, s);
+    const even = getDrome(i + 1, i, s);
 
-    let curMax = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+    const curMax = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
     max = max[1] - max[0] > curMax[1] - curMax[0] ? max : curMax;
   }
 
