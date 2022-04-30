fb64bd159a6cacf634235de9a70a4ac9f5e49419
petitoff <petitoff@outlook.com>
2022-04-30 15:31:07 +0200

    Add description

--- LeetCode/Reverse Integer.js
+++ LeetCode/Reverse Integer.js
@@ -1,3 +1,9 @@
+/*
+Link: https://leetcode.com/problems/reverse-integer/
+Description:
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes 
+the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+*/
 const reverse = function (x) {
   let str = x.toString();
 
